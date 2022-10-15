e53ff089fe99d648756c202885c2ca89bf4cecab
b1y9mn <b1y9mn@gmail.com>
2022-10-15 21:56:44 +0900

    chore: 구현하지 못한 글자 움직이는 기능

--- 1. 움직이는 글자/main.js
+++ 1. 움직이는 글자/main.js
@@ -8,12 +8,10 @@ function printInput() {
   function disappearPrint() {
     let inputValue = document.getElementById('input').value;
     for (let i=0; i<inputValue.length; i++) {
-      if (Math.floor(i/2) % 2 != 0) {
-        console.log(inputValue[i]);
-        inputValue = inputValue.replace(inputValue[i], ' ');
+        inputValue = inputValue.replace(inputValue[4*i+2], ' ').replace(inputValue[4*i+3], ' ');
         document.getElementById("result").textContent = inputValue;
+        console.log(document.getElementById("result"));
       }
     }
-  };
   disappearPrint();
 }
