f9acace2a43b4d697d27bd4d779a4c646367abcf
biyamn <b1y9mn@gmail.com>
2022-10-16 12:12:15 +0900

    chore: 주석

--- 1. test/test.js
+++
@@ -1,8 +0,0 @@
-let inputValue = '1234567890가나다라마바사'
-
-for (let i=0; i<inputValue.length; i++) {
-  if (i%4 === 0) {
-    inputValue = inputValue.replace(inputValue[i], ' ').replace(inputValue[i+1], ' ');
-  }
-  console.log(inputValue);
-}
--- 1. 움직이는 글자/main.js
+++ 1. 움직이는 글자/main.js
@@ -1,4 +1,5 @@
 function printInput() {
+  // 입력받은 값을 아래의 div에 똑같이 써준다.
   function ordinaryPrint() {
     let inputValue = document.getElementById('input').value;
     document.getElementById("result").textContent = inputValue;
@@ -6,12 +7,20 @@ function printInput() {
   ordinaryPrint();
 
   function disappearPrint() {
-    let inputValue = document.getElementById('input').value;
-    for (let i=0; i<inputValue.length; i++) {
-        inputValue = inputValue.replace(inputValue[4*i+2], ' ').replace(inputValue[4*i+3], ' ');
-        document.getElementById("result").textContent = inputValue;
-        console.log(document.getElementById("result"));
-      }
-    }
+    // 무한 반복한다. 새로운 값이 입력되기 전까지
+    // while (새로운 값 입력되기 전)) {
+      let inputValue = document.getElementById('input').value;
+      // 이중 for문 시작,,, 오른쪽으로 움직이도록 인덱스를 증가시킨다? 
+      // 근데 끝을 지나면 앞으로 다시 돌아오게 해야 함
+      // for(let i=0; i<inputValue.length; i++) {
+      // 2글자 간격으로 2개씩 글자를 스페이스로 대체한다
+        for (let j=0; j<inputValue.length; j++) {
+            inputValue = inputValue.replace(inputValue[4*j+2], ' ').replace(inputValue[4*j+3], ' ');
+            console.log(inputValue)
+            document.getElementById("result").textContent = inputValue;
+          }
+        // }
+    // }
+  }
   disappearPrint();
 }
